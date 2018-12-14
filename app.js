2dc3165f085f253c242d1e8767620c47f3fa78d4
lichRuslan <lichRuslan@users.noreply.github.com>
2018-12-15 01:33:00 +0600

    Вроде всё почитил, можно настраивать дизайн

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let Post = require('./models/post');
 let path = require('path');
 let app = express();
 
@@ -8,27 +7,13 @@ let app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/javascripts', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
-
-// var arr = ['Hello', 'world', 'lol'];
 app.get('/', function (req, res) {
-  Post.find({}).then(posts => {
-    res.render('index', {posts: posts});
-  });
-  //res.render('index', {arr: arr});
+  res.render('index');
 });
 app.get('/create', function (req, res) {
   res.render('create');
-});
-app.post('/create', function (req, res) {
-  console.log(req.body);
-  var {title, body} = req.body;
-  Post.create({
-    title: title,
-    body:body
-  }).then(post => console.log(post.id));
-
-  res.redirect('/');
 });
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     MONGO_URL: 'mongodb://localhost/blog',
-    PORT : process.env.PORT || 5001
+    PORT : process.env.PORT || 5000
     
 }
