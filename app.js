782e5b60fbdeba733577163831e3bb2d71b17177
lichRuslan <lichRuslan@users.noreply.github.com>
2018-12-15 00:19:27 +0600

    Стили и тп ОК

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let Post = require('./models/post');
+let path = require('path');
 let app = express();
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // var arr = ['Hello', 'world', 'lol'];
 app.get('/', function (req, res) {
   Post.find({}).then(posts => {
