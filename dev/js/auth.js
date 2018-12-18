844780d5d177d7ff9c60c5c3821bb12e0a6b4ffa
lichruslan <lichruslan@github.com>
2018-12-19 01:22:29 +0600

    Регистрацию проктический сделал, осталось пофиксить некоторые вещи, регистрация и сохронения в базу есть, проверка есть

--- dev/js/auth.js
+++ dev/js/auth.js
@@ -1,46 +1,54 @@
-/* eslint-disable */
-$(function(){
-    //toggle
+/* eslint-disable no-undef */
+$(function() {
+    // toggle
     var flag = true;
-    $('.switch-button').on('click', function(e){
-        e.preventDefault(); 
-        if(flag){
-            flag = false;
-            $('.register').show('slow');
-            $('.login').hide();
-        }else{
-            flag = true;
-            $('.login').show('slow');
-            $('.register').hide();
-        };
+    $('.switch-button').on('click', function(e) {
+      e.preventDefault();
+  
+      if (flag) {
+        flag = false;
+        $('.register').show('slow');
+        $('.login').hide();
+      } else {
+        flag = true;
+        $('.login').show('slow');
+        $('.register').hide();
+      }
     });
+  
+    // clear
+    $('input').on('focus', function() {
+      $('p.error').remove();
+      $('input').removeClass('error');
+    });
+  
+    // register
     $('.register-button').on('click', function(e) {
-        e.preventDefault();
-    
-        var data = {
-          login: $('#register-login').val(),
-          password: $('#register-password').val(),
-          passwordConfirm: $('#register-password-confirm').val()
-        };
-        //Функция отправки POST в базу 
-        $.ajax({
-            type: 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json',
-            url: '/api/auth/register'
-        }).done(function(data){
-            console.log(data);
-        });
+      e.preventDefault();
+  
+      var data = {
+        login: $('#register-login').val(),
+        password: $('#register-password').val(),
+        passwordConfirm: $('#register-password-confirm').val()
+      };
+  
+      $.ajax({
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        url: '/api/auth/register'
+      }).done(function(data) {
+        if (!data.ok) {
+          $('.register h2').after('<p class="error">' + data.error + '</p>');
+          if (data.fields) {
+            data.fields.forEach(function(item) {
+              $('input[name=' + item + ']').addClass('error');
+            });
+          }
+        } else {
+          $('.register h2').after('<p class="success">Отлично!</p>');
+        }
+      });
     });
-});
-
-// // register
-// $('.register-button').on('click', function(e) {
-//     e.preventDefault();
-
-//     var data = {
-//       login: $('#register-login').val(),
-//       password: $('#register-password').val(),
-//       passwordConfirm: $('#register-password-confirm').val()
-//     };
-// });
+  });
+   /* eslint-disable no-undef */
